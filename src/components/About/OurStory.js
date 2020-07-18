9bcac6d1a6efce530b48f35def25e56066d79052
paulazoo <paulazhu@college.harvard.edu>
2020-07-18 01:31:13 -0400

    Start mission purpose and our story page

--- src/components/About/OurStory.js
+++ src/components/About/OurStory.js
@@ -1,26 +1,134 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import {
-  Button,
-  TextField,
-  Grid,
-  CardContent,
+  Divider,
+  Card,
   Typography,
+  Box,
+  Grid,
+  Paper,
+  Button,
 } from '@material-ui/core';
-
-// Theme
-import { makeStyles } from '@material-ui/core/styles';
 
 // Redux
 import { connect } from 'react-redux';
-import { userLogout, setUser } from '../../store/actions/index';
+
+// Theme
+import { theme } from '../../theme';
+import { makeStyles } from '@material-ui/core/styles';
 
 // Custom Components
 import WordDivider from '../Shared/WordDivider';
+import {
+  executiveTeam,
+  outreachTeam,
+  recruitmentDirectors,
+  programTeam,
+  graphicsMarketingTeam,
+  curriculumTeam,
+  technicalTeam,
+} from './teamMembers';
 
 const useStyles = makeStyles((theme) => ({
+  googleButton: {
+    paddingLeft: '22px',
+  },
+  prefix: {
+    paddingLeft: '8px',
+    color: '#005086',
+    fontSize: '200%',
+  },
+  suffix: {
+    color: '#61CCD7',
+    fontSize: '200%',
+  },
+  description: {
+    paddingLeft: '8px',
+    color: '#777272',
+  },
+  navleft: {
+    color: '#005086',
+  },
+  navright: {
+    color: '#005086',
+  },
+  button: {
+    minWidth: 200,
+  },
+  h5: {
+    marginTop: theme.spacing(4),
+    [theme.breakpoints.up('sm')]: {
+      marginTop: theme.spacing(25),
+    },
+  },
+  title: {
+    paddingLeft: '8px',
+  },
+  more: {
+    marginTop: theme.spacing(2),
+  },
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  imgResponsive: {
+    height: 'auto',
+    width: 'auto',
+    maxHeight: '40px',
+    maxWidth: '250px',
+  },
+  img: {
+    width: '30vh',
+    borderRadius: '50%',
+  },
+  headText: {
+    fontWeight: 'bold',
+    color: theme.palette.common.black,
+    fontSize: 32,
+  },
+  headTextContainer: {
+    textAlign: 'center',
+  },
+  memberItemContainer: {},
+  memberItem: {
+    backgroundColor: theme.palette.common.white,
+    padding: theme.spacing(2),
+  },
+  totalGrid: {
+    padding: theme.spacing(4),
+  },
+  memberName: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  memberTeamName: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: theme.palette.primary.main,
+  },
+  memberPosition: {
+    fontSize: 16,
+    color: theme.palette.primary.main,
+  },
+  memberText: {
+    color: theme.palette.common.gray,
+    fontSize: 14,
+  },
+  memberTextContainer: {
+    textAlign: 'left',
+  },
   wordDivider: {
     fontWeight: 'bold',
     color: theme.palette.common.gray,
+  },
+  teamName: {
+    fontWeight: 'bold',
+    fontSize: 32,
+    color: theme.palette.common.white,
+    padding: 0,
+  },
+  teamNameContainer: {
+    padding: '0 !important',
   },
 }));
 
@@ -34,16 +142,56 @@ function OurStory(props) {
           Our Story
         </Typography>
       </WordDivider>
+      <Card
+        style={{
+          margin: 0,
+          marginRight: theme.spacing(6),
+          marginLeft: theme.spacing(6),
+          marginBottom: theme.spacing(6),
+          backgroundColor: theme.palette.common.teamBlue,
+        }}
+      >
+        <Grid container spacing={0}>
+          <Grid item xs={12} sm={12} height='100%' width='80%'>
+            <Grid
+              container
+              direction='row'
+              justify='center'
+              alignItems='center'
+              spacing={4}
+              className={classes.totalGrid}
+            >
+              <Grid item xs={12} className={classes.teamNameContainer}>
+                <Grid
+                  container
+                  direction='row'
+                  justify='center'
+                  className={classes.teamNameContainer}
+                >
+                  <Grid item className={classes.teamNameContainer}>
+                    <Typography className={classes.teamName}>
+                      {'ou rtory '}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={12} className={classes.memberItemContainer}>
+                <Grid
+                  container
+                  justify='center'
+                  alignItems='center'
+                  direction='row'
+                  className={classes.memberItem}
+                >
+                  hi
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Card>
     </>
   );
 }
 
-const mapStateToProps = (state) => ({});
-
-function mapDispatchToProps(dispatch) {
-  return {
-    userLogout: () => dispatch(userLogout()),
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(OurStory);
+export default OurStory;
