102e3bfae3bed91fc7bf25a75ddffa528b9169cd
paulazoo <paulazhu@college.harvard.edu>
2020-07-17 23:43:46 -0400

    All logos up and linking!

--- src/components/Landing/AsSeenIn.js
+++ src/components/Landing/AsSeenIn.js
@@ -37,12 +37,32 @@ function AsSeenIn(props) {
           <Typography className={classes.headText}>AS SEEN IN</Typography>
         </Grid>
         <Grid item>
-          <img
-            alt='Honolulu Civil Beat'
-            height={128}
-            width={128}
-            src={require('../../assets/AsSeenIn/HonoluluCivilBeat.PNG')}
-          />
+          <a
+            rel='noreferrer'
+            target='_blank'
+            href='https://www.civilbeat.org/2020/06/we-must-reimagine-college-guidance-programs/?fbclid=IwAR2Tad_5RBylBf9U99mrej4n4G5f2-EfiZzlHLlFu_R3DIWk4XVrKD7PvJE'
+          >
+            <img
+              alt='Honolulu Civil Beat'
+              height={128}
+              width={128}
+              src={require('../../assets/AsSeenIn/HonoluluCivilBeat.PNG')}
+            />
+          </a>
+        </Grid>
+        <Grid item>
+          <a
+            rel='noreferrer'
+            target='_blank'
+            href='http://casarevista.com/5192/news/we-must-refocus-on-college-guidance-programs/?fbclid=IwAR2lOx_uw8UuV-OtNS58TbNh4jcsY4I-oTithRW0i1o41rRejIPukDyX7W0'
+          >
+            <img
+              alt='TheCasaRevista'
+              height={128}
+              width={128}
+              src={require('../../assets/AsSeenIn/TheCasaRevista.PNG')}
+            />
+          </a>
         </Grid>
         <Grid item xs={12} />
       </Grid>
--- src/components/Landing/Partners.js
+++ src/components/Landing/Partners.js
@@ -37,60 +37,122 @@ function Partners(props) {
           <Typography className={classes.headText}>PARTNERS</Typography>
         </Grid>
         <Grid item>
-          <img
-            alt='College Essay Guy'
-            height={128}
-            width={128}
-            src={require('../../assets/Partners/CollegeEssayGuy.PNG')}
-          />
+          <a
+            rel='noreferrer'
+            target='_blank'
+            href='https://www.collegeessayguy.com/'
+          >
+            <img
+              alt='College Essay Guy'
+              height={128}
+              width={128}
+              src={require('../../assets/Partners/CollegeEssayGuy.PNG')}
+            />
+          </a>
         </Grid>
         <Grid item>
-          <img
-            alt='Dear Asian Youth'
-            height={128}
-            width={128}
-            src={require('../../assets/Partners/DearAsianYouth.PNG')}
-          />
+          <a
+            rel='noreferrer'
+            target='_blank'
+            href='https://www.dearasianyouth.org/home/'
+          >
+            <img
+              alt='Dear Asian Youth'
+              height={128}
+              width={128}
+              src={require('../../assets/Partners/DearAsianYouth.PNG')}
+            />
+          </a>
         </Grid>
         <Grid item>
-          <img
-            alt='Destined Doctor'
-            height={128}
-            width={128}
-            src={require('../../assets/Partners/DestinedDoctor.PNG')}
-          />
+          <a
+            rel='noreferrer'
+            target='_blank'
+            href='https://destineddoctors.wixsite.com/website'
+          >
+            <img
+              alt='Destined Doctors'
+              height={128}
+              width={128}
+              src={require('../../assets/Partners/DestinedDoctors.PNG')}
+            />
+          </a>
         </Grid>
         <Grid item>
-          <img
-            alt='Socrates Post'
-            height={128}
-            width={128}
-            src={require('../../assets/Partners/SocratesPost.PNG')}
-          />
+          <a rel='noreferrer' target='_blank' href='https://socratespost.com/'>
+            <img
+              alt='Socrates Post'
+              height={128}
+              width={128}
+              src={require('../../assets/Partners/SocratesPost.PNG')}
+            />
+          </a>
         </Grid>
         <Grid item>
-          <img
-            alt='We Rise Above'
-            height={128}
-            width={128}
-            src={require('../../assets/Partners/WeRiseAbove.PNG')}
-          />
+          <a rel='noreferrer' target='_blank' href='https://weriseabove.org/'>
+            <img
+              alt='We Rise Above'
+              height={128}
+              width={128}
+              src={require('../../assets/Partners/WeRiseAbove.PNG')}
+            />
+          </a>
         </Grid>
         <Grid item>
-          <img
-            alt='Wonsulting'
-            height={128}
-            width={128}
-            src={require('../../assets/Partners/Wonsulting.PNG')}
-          />
+          <a
+            rel='noreferrer'
+            target='_blank'
+            href='https://www.wonsulting.com/'
+          >
+            <img
+              alt='Wonsulting'
+              height={128}
+              width={128}
+              src={require('../../assets/Partners/Wonsulting.PNG')}
+            />
+          </a>
         </Grid>
         <Grid item>
-          <img
-            alt='YMCA Honolulu'
-            height={128}
-            width={128}
-            src={require('../../assets/Partners/YMCAHonolulu.PNG')}
-          />
+          <a
+            rel='noreferrer'
+            target='_blank'
+            href='https://www.ymcahonolulu.org/'
+          >
+            <img
+              alt='YMCA Honolulu'
+              height={128}
+              width={128}
+              src={require('../../assets/Partners/YMCAHonolulu.PNG')}
+            />
+          </a>
+        </Grid>
+        <Grid item>
+          <a
+            rel='noreferrer'
+            target='_blank'
+            href='https://girlgeniusmag.tech/'
+          >
+            <img
+              alt='Girl Genius Magazine'
+              height={128}
+              width={128}
+              src={require('../../assets/Partners/GirlGeniusMagazine.PNG')}
+            />
+          </a>
+        </Grid>
+        <Grid item>
+          <a
+            rel='noreferrer'
+            target='_blank'
+            href='https://www.girltechboss.com/'
+          >
+            <img
+              alt='Girl Tech Boss'
+              height={128}
+              width={128}
+              src={require('../../assets/Partners/GirlTechBoss.PNG')}
+            />
+          </a>
         </Grid>
         <Grid item xs={12} />
       </Grid>
