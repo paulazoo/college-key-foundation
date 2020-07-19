6b57c1bee0f50928bb9848a05a7f27409a3b7504
paulazoo <paulazhu@college.harvard.edu>
2020-07-18 22:51:17 -0400

    Mobile apply

--- src/components/Apply/Apply.js
+++ src/components/Apply/Apply.js
@@ -35,6 +35,11 @@ const useStyles = makeStyles((theme) => ({
     marginTop: 0,
     padding: theme.spacing(8),
     height: '40vh',
+    [theme.breakpoints.down('sm')]: {
+      padding: theme.spacing(3),
+      margin: theme.spacing(1),
+      marginTop: '15vh',
+    },
   },
   applyTextContainer: {
     textAlign: 'center',
@@ -42,23 +47,35 @@ const useStyles = makeStyles((theme) => ({
   applyText: {
     fontWeight: 'bold',
     fontSize: 28,
+    [theme.breakpoints.down('sm')]: {
+      fontSize: 24,
+    },
   },
   applyButton: {
     fontWeight: 'bold',
     fontSize: 24,
     [theme.breakpoints.down('sm')]: {
-      fontSize: 12,
+      fontSize: 20,
     },
   },
   applyDivider: {
     margin: theme.spacing(4),
     height: '20vh',
+    [theme.breakpoints.down('sm')]: {
+      margin: theme.spacing(1),
+    },
   },
   dueDate: {
     fontWeight: 'bold',
     fontSize: 18,
     [theme.breakpoints.down('sm')]: {
       fontSize: 10,
+    },
+  },
+  main: {
+    margin: 0,
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '15vh',
     },
   },
 }));
@@ -75,8 +92,14 @@ function Apply(props) {
         </Typography>
       </WordDivider>
 
-      <Grid container direction='row' alignItems='center' justify='center'>
-        <Grid item xs={8}>
+      <Grid
+        container
+        direction='row'
+        alignItems='center'
+        justify='center'
+        className={classes.main}
+      >
+        <Grid item xs={12} md={8}>
           <Card className={classes.applyCard}>
             <Grid
               container
