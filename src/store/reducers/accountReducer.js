44a0da129d3436edf5794206195a7d6e31992279
paulazoo <paulazhu@college.harvard.edu>
2020-07-30 18:58:09 -0400

    set is master accont reducer

--- src/store/reducers/accountReducer.js
+++ src/store/reducers/accountReducer.js
@@ -2,6 +2,23 @@ import { initialState } from './initialState';
 
 export default function accountReducer(state = initialState.account, action) {
   switch (action.type) {
+    case 'SET_ISMASTER':
+      let newIsMaster = false;
+      if (
+        action.payload === 'paulazhu@college.harvard.edu' ||
+        action.payload === 'collegekeyfoundation@gmail.com' ||
+        action.payload === 'snalani731@gmail.com' ||
+        action.payload === 'llin1@college.harvard.edu' ||
+        action.payload === 'lindalin2812@gmail.com'
+      ) {
+        newIsMaster = true;
+      }
+
+      return {
+        ...state,
+        isMaster: newIsMaster,
+      };
+
     case 'SET_ACCOUNT':
       return action.payload;
 
